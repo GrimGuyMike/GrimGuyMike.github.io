6a8c5dde7b6328e3418154752651d5d5077dd9e7
GrimGuyMike <ggm_job@mail.ru>
2021-09-28 12:16:40 +0500

    added wrapper main function

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const header = document.querySelector('header');
-const greetings = document.getElementById('greetings');
-const navLinks = document.querySelectorAll('.navbar a');
-
-// INTERSECTION OBSERVER SETUP
-
-const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-        header.classList.toggle('sticky', !entry.isIntersecting);
+function main() {
+    const header = document.querySelector('header');
+    const greetings = document.getElementById('greetings');
+    const navLinks = document.querySelectorAll('.navbar a');
+    
+    // INTERSECTION OBSERVER SETUP
+    
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            header.classList.toggle('sticky', !entry.isIntersecting);
+        });
+    }, { threshold: .25 });
+    
+    observer.observe(greetings);
+    
+    // NAVLINKS BEHAVIOR
+    
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+        });
     });
-}, { threshold: .25 });
+};
 
-observer.observe(greetings);
-
-// NAVLINKS BEHAVIOR
-
-navLinks.forEach(link => {
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
-    });
-});
+main();
